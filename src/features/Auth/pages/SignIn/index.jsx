9178ecb758a8de01828abeaf97fd236f98da1fa6
thuccv98 <thuccv98@gmail.com>
2021-09-02 23:00:21 +0700

    Add login functionality with firebaseUI

---
+++ src/features/Auth/pages/SignIn/index.jsx
@@ -0,0 +1,23 @@
+import firebase from 'firebase';
+import { StyledFirebaseAuth } from 'react-firebaseui';
+
+const uiConfig = {
+  signInFlow: 'redirect',
+  signInSuccessUrl: '/photos',
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+};
+
+const SignIn = () => {
+  return (
+    <div>
+      <div className="text-center">
+        <h2>Login Form</h2>
+        <p>or login with social accounts</p>
+      </div>
+
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    </div>
+  );
+};
+
+export default SignIn;
